2d1c8d8693ee377df33c4193521a910716a22297
Hafi <mufaddholhafi26@gmail.com>
2024-11-24 18:35:38 +0800

    <feat> add isNew to jwt token payload

--- src/utils/tokenHelper.js
+++ src/utils/tokenHelper.js
@@ -6,7 +6,7 @@ const { json } = require('body-parser');
 require('dotenv').config();
 
 const generateAccessToken = (user) => {
-  return jwt.sign({ id: user.id, name: user.name }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  return jwt.sign({ id: user.id, name: user.name ,isNew : user.level==null }, process.env.JWT_SECRET, { expiresIn: '1h' });
 };
 
 const generateRefreshToken = async (user) => {
